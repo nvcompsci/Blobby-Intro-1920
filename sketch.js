5949c04b91e2cbbaa57539aea143d1bcddb23140
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2019-09-20 10:14:30 -0400

    add player and key events

--- sketch.js
+++ sketch.js
@@ -1,6 +1,17 @@
 let x = 1
 let y = 50
 
+//Create player object
+let player = {
+  x: 0,
+  y: 0,
+  width: 50,
+  height: 30,
+  speed: 5,
+  vx: 0,
+  vy: 0
+}
+
 function setup() {
   createCanvas(400, 400);
   frameRate(24)
@@ -10,9 +21,29 @@ function draw() {
   background(220);
   stroke("red")
   fill("blue")
-  rect(x,y,50,50)
-  x += 3
-  y += 1
+  rect(player.x, player.y, player.width, player.height)
+  player.x += player.vx
+  player.y += player.vy
   fill("green")
-  circle(mouseX,mouseY,30)
+  circle(mouseX, mouseY, 30)
+}
+
+function keyPressed() {
+  if (keyCode === RIGHT_ARROW) {
+    player.vx = player.speed
+  }
+  else if (keyCode === LEFT_ARROW) {
+    player.vx = -player.speed
+  }
+  else if (keyCode === UP_ARROW) {
+    player.vy = -player.speed
+  }
+  else if (keyCode === DOWN_ARROW) {
+    player.vy = player.speed
+  }
+}
+
+function keyReleased() {
+  player.vx = 0
+  player.vy = 0
 }
