92bf8fdb95812cc1c3f47a09baf763df58fd4a82
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2019-09-30 10:29:28 -0400

    add enemy, start rainbow challenge

--- sketch.js
+++ sketch.js
@@ -12,6 +12,16 @@ let player = {
   vy: 0
 }
 
+let enemy = {
+  x: 400,
+  y: 10,
+  width: 50,
+  height: 30,
+  speed: 5,
+  vx: -3,
+  vy: 0
+}
+
 function setup() {
   createCanvas(400, 400);
   frameRate(24)
@@ -22,10 +32,25 @@ function draw() {
   stroke("red")
   fill("blue")
   rect(player.x, player.y, player.width, player.height)
+  
+  if (enemy.x > 300) {
+     fill("red") 
+  }
+  else if (enemy.x > 200) {
+      fill("orange")
+  }
+  //yellow, green, blue
+  else {
+    fill("purple")
+  }
+    
+  circle(enemy.x, enemy.y, enemy.width, enemy.height)
   player.x += player.vx
   player.y += player.vy
-  fill("green")
-  circle(mouseX, mouseY, 30)
+  enemy.x += enemy.vx
+  enemy.y += enemy.vy
+  //fill("green")
+  //circle(mouseX, mouseY, 30)
 }
 
 function keyPressed() {
